032e4e0cc3804064053c479f396f22f3eed5564b
Abayomi Aluko <franicons@gmail.com>
2023-07-11 01:45:01 +0100

    feat: development

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -2,6 +2,9 @@
 echo "Running composer"
 composer global require hirak/prestissimo
 composer install --working-dir=/var/www/html
+echo "Running npm"
+npm install
+npm run prod
 
 echo "Caching config..."
 php artisan config:cache
@@ -11,6 +14,7 @@ php artisan route:cache
 
 echo "Running migrations..."
 php artisan migrate:refresh --seed --force
+
 
 
 
