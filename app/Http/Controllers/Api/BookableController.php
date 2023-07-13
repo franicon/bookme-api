798bc45dfb3569823b20fabef6ebb5111c5f6488
Abayomi Aluko <franicons@gmail.com>
2023-07-13 15:48:58 +0100

    feat: development

--- app/Http/Controllers/Api/BookableController.php
+++ app/Http/Controllers/Api/BookableController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Resources\BookableIndexResource;
+use App\Http\Resources\BookableShowResource;
 use App\Models\Bookable;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -9,11 +11,13 @@ use App\Http\Controllers\Controller;
 class BookableController extends Controller {
     // Return all Bookables
     public function index() {
-        return Bookable::all();
+        return BookableIndexResource::collection(
+            Bookable::all()
+        );
     }
 
     // Return all Single Bookable with ID
     public function show($id) {
-        return Bookable::findOrFail($id);
+        return new BookableShowResource(Bookable::findOrFail($id));
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\Bookable;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\BookableController;
@@ -8,7 +7,8 @@ use App\Http\Controllers\Api\BookableController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+//
+//Route::get('bookables', [BookableController::class, 'index']);
+//Route::get('bookables/{id}', [BookableController::class, 'show']);
 
-Route::get('bookables', [BookableController::class, 'index']);
-Route::get('bookables/{id}', [BookableController::class, 'show']);
-
+Route::apiResource('bookables', BookableController::class)->only('index', 'show');
