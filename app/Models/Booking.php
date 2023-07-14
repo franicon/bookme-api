aaa8b8ad33bb95585810459436b4ca8dccbad2c2
Abayomi Aluko <franicons@gmail.com>
2023-07-14 16:56:15 +0100

    feat: development

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -2,15 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder as Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Booking extends Model {
     use HasFactory;
 
-    protected $fillable = [];
+    protected $fillable = ['from', 'to'];
 
     public function bookable() {
         return $this->belongsTo(Bookable::class);
+    }
+
+    public function scopeBetweenDates(Builder $query, $from, $to): Builder {
+        return $query->where('to', '>=', $from)->where('from', '<=', $to);
     }
 }
