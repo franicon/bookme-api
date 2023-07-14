d3b5a7fdd594b504ee52145932a9ccf94b17be33
Abayomi Aluko <franicons@gmail.com>
2023-07-14 15:21:55 +0100

    feat: development

--- database/seeders/BookingsSeeder.php
+++ database/seeders/BookingsSeeder.php
@@ -14,16 +14,18 @@ class BookingsSeeder extends Seeder {
             $booking = Booking::factory()->make();
             $bookings = collect([$booking]);
 
-            for ($i=0; $i < random_int(1, 20); $i++) {
+            for ($i = 0; $i < random_int(1, 20); $i++) {
                 $from = (clone $booking->to)->addDays(random_int(1, 14));
-                $to =(clone $from)->addDays(random_int(0, 14));
+                $to = (clone $from)->addDays(random_int(0, 14));
 
-                $booking = Bookable::make([
+                $booking = Booking::make([
                     'from' => $from,
-                    'to' => $to
+                    'to' => $to,
                 ]);
+
                 $bookings->push($booking);
             }
+
             $bookable->bookings()->saveMany($bookings);
         });
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,5 @@ class DatabaseSeeder extends Seeder {
 //         \App\Models\User::factory(10)->create();
             $this->call([BookableSeeder::class]);
             $this->call([BookingsSeeder::class]);
-
     }
 }
